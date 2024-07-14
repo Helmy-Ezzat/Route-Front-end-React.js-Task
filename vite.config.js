e3f6647de3528faf1ab7243e7f9fbe78006aff16
Helmy Ezzat <hellmyezzat@gmail.com>
2024-07-14 03:35:51 +0300

    fix Vite3

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js or equivalent
-export default {
-  build: {
-    rollupOptions: {
-      external: ['react-paginate'],
-    },
-  },
-}
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/Route-Front-end-React.js-Task/',
+})
